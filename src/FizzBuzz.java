1738d343c26cab83349c88c7ecf6549819391e24
MionaTanaka <104816812+Miomon3@users.noreply.github.com>
2022-05-22 02:31:07 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,19 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		for (int number = 1; number <= 100; number++) {
+			
+			if (number % 3 == 0 && number % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (number % 5 == 0) {
+				System.out.println("Bizz");
+			} else {
+				System.out.println(number);
+			}
+			
+		}
 
 	}
 
